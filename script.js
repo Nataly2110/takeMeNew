c09464fd54d814fbcd2f8f96589660ea46fb4827
Nataly2110 <n.novytskaya@gmail.com>
2021-12-14 12:33:00 +0200

    сжатые svg для футера

--- script.js
+++ script.js
@@ -66,6 +66,16 @@ if (menuLinksDataGoto.length > 0) {
 
          e.preventDefault();
       }
-      
    }
+}
+
+//меню-бургер
+const iconMenu = document.querySelector(".menu__icon");
+if (iconMenu) {
+   const menuBody = document.querySelector(".menu__body");
+   iconMenu.addEventListener("click", function(e) {
+      document.body.classList.toggle("_lock")
+      iconMenu.classList.toggle("_active");
+      menuBody.classList.toggle("_active");
+   })
 }
