b872f9bb1cacb309424bb373b9869eb55fff8293
Nataly2110 <n.novytskaya@gmail.com>
2021-12-16 16:49:19 +0200

    доработано меню-бургер

--- script.js
+++ script.js
@@ -44,6 +44,18 @@ if (isMobile.any()) {
    document.body.classList.add("_pc");
 }
 
+//меню-бургер
+const iconMenu = document.querySelector(".menu__icon");
+const menuBody = document.querySelector(".menu__body");
+
+if (iconMenu) {
+   iconMenu.addEventListener("click", function (e) {
+      document.body.classList.toggle("_lock")
+      iconMenu.classList.toggle("_active");
+      menuBody.classList.toggle("_active");
+   })
+}
+
 //прокрутка при клике
 
 const menuLinksDataGoto = document.querySelectorAll(".menu__link[data-goto]");
@@ -59,6 +71,12 @@ if (menuLinksDataGoto.length > 0) {
          const gotoBlock = document.querySelector(menuLink.dataset.goto);
          const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector("header").offsetHeight;
 
+         if (iconMenu.classList.contains("_active")) {
+            document.body.classList.remove("_lock")
+            iconMenu.classList.remove("_active");
+            menuBody.classList.remove("_active");
+         }
+
          window.scrollTo({
             top: gotoBlockValue,
             behavior: "smooth"
@@ -69,13 +87,3 @@ if (menuLinksDataGoto.length > 0) {
    }
 }
 
-//меню-бургер
-const iconMenu = document.querySelector(".menu__icon");
-if (iconMenu) {
-   const menuBody = document.querySelector(".menu__body");
-   iconMenu.addEventListener("click", function(e) {
-      document.body.classList.toggle("_lock")
-      iconMenu.classList.toggle("_active");
-      menuBody.classList.toggle("_active");
-   })
-}
